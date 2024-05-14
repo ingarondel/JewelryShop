28506b4ac4b8b8d97149365c08994c70409e8df7
ingarondel <90716651+ingarondel@users.noreply.github.com>
2024-05-14 11:58:49 -0700

    fix: config assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-
+Rails.application.config.assets.precompile += %w[application.js application.css]
 Rails.application.config.assets.precompile += %w[bootstrap.scss]
 Rails.application.config.assets.precompile += %w[style.scss]
 Rails.application.config.assets.precompile += %w[memenu.scss]
