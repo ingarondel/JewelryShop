5fd5549f3b9300a1d37ebc446ec7207f31349142
ingarondel <inga404@icloud.com>
2024-07-29 12:37:14 -0700

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,6 @@ bundle install
 yarn install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rails db:drop
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
 bundle exec rails db:create
 bundle exec rails db:migrate
