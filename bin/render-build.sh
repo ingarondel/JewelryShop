7f26ab5a26fdad275107d38601a5b1efcff9936e
ingarondel <inga404@icloud.com>
2024-07-29 12:21:23 -0700

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,5 @@ yarn install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+
+Category.destroy_all
--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,20 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   use_doorkeeper
   devise_for :users
-  
+
 
 resources :product, only: [:show]
 
 resources :category, only: [:show]
 
-resources :tokens, only: [:new]
-
-post '/generate_token', to: 'tokens#generate_token', as: 'generate_token'
-
-resource :cart, only: %i[destroy show] do  
+resource :cart, only: %i[destroy show] do
 resources :items, only: %i[destroy create]
 end
 
 resources :search, only: [:index]
 
-namespace :api do 
-  namespace :v1 do  
+namespace :api do
+  namespace :v1 do
    resources :profile, only: [:index]
    resources :products, only: [:index, :show, :destroy, :update, :create]
    defaults format: :json do
