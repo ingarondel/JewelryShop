e4286c8e86560013915f3f84cc8ab53843c3fe85
ingarondel <inga404@icloud.com>
2024-07-29 12:28:48 -0700

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,8 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
 
+bundle exec rails console
+
 categories_to_keep = Category.limit(14)
 
 Category.where.not(id: categories_to_keep.pluck(:id)).destroy_all
